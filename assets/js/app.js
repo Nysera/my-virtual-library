62b0111876ac6d2faae075371e87c7c69c3e1502
Nysera <73109965+Nysera@users.noreply.github.com>
2020-12-01 21:21:22 +1000

    Add closeModal function for closing modals

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 const infoAddBookBtn = document.querySelector("#info-add-book");
 const overlayBackdrop = document.querySelector(".overlay-backdrop");
 const formModal = document.querySelector(".form-modal");
+const formWrapper = document.querySelector(".form-wrapper");
 const addBookModal = document.querySelector("#add-book-modal");
+const closeModalBtn = document.querySelector(".close-modal");
 
 
 
@@ -19,6 +21,22 @@ const openModal = function() {
     }, 25);
 };
 
+const closeModal = function() {
+    overlayBackdrop.classList.remove("active");
+    formWrapper.classList.remove("active");
+    setTimeout(function() {
+        formModal.style.display = "none";
+        formModal.style.opacity = "0";
+        formWrapper.style.display = "none";
+    }, 400);
+};
+
 
 
 infoAddBookBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+formModal.addEventListener("click", function(event){
+    if (event.target === this) {
+        closeModal();
+    }
+});
