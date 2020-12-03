78452453a8d04b38c69ad31a3bfcc96bb33d2449
Nysera <73109965+Nysera@users.noreply.github.com>
2020-12-04 09:10:17 +1000

    Changed how I planned on doing the modals, added getBookValues function to get form values

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,49 @@
-const infoAddBookBtn = document.querySelector("#info-add-book");
-const overlayBackdrop = document.querySelector(".overlay-backdrop");
+const addBook = document.querySelector("#add-book");
 const formModal = document.querySelector(".form-modal");
-const formWrapper = document.querySelector(".form-wrapper");
-const addBookModal = document.querySelector("#add-book-modal");
-const closeModalBtn = document.querySelector(".close-modal");
+const formModalInner = document.querySelector(".form-modal_inner")
+const overlayBackdrop = document.querySelector(".overlay-backdrop");
 
 
+const getBookValues = function() {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const imageUrl = document.querySelector("#image-url").value;
+    const readStatus = document.querySelector("#read-status").value;
+    
+    closeModal();
+};
 
 const openModal = function() {
+    formModalInner.innerHTML = addBookTemplate();
     formModal.style.display = "flex";
     formModal.style.opacity = "1";
-    if (this.id === "info-add-book") {
-        addBookModal.style.display = "block";
-    }
-    setTimeout( () => {
+    setTimeout(function() {
         overlayBackdrop.classList.add("active");
-        if (this.id === "info-add-book") {
-            addBookModal.classList.add("active");
-        }
+        formModalInner.classList.add("active");
     }, 25);
 };
 
 const closeModal = function() {
     overlayBackdrop.classList.remove("active");
-    formWrapper.classList.remove("active");
+    formModalInner.classList.remove("active");
     setTimeout(function() {
         formModal.style.display = "none";
         formModal.style.opacity = "0";
-        formWrapper.style.display = "none";
+        formModalInner.innerHTML = "";
     }, 400);
 };
 
 
 
-infoAddBookBtn.addEventListener("click", openModal);
-closeModalBtn.addEventListener("click", closeModal);
-formModal.addEventListener("click", function(event){
-    if (event.target === this) {
+addBook.addEventListener("click", openModal);
+document.addEventListener("click", function(event) {
+    const { target } = event;
+
+    if (target.id === "close-modal") {
         closeModal();
+    } else if (target.id === "submit-book") {
+        event.preventDefault();
+        getBookValues();
     }
 });
