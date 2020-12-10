f39ae87f9715b5e8610394684894628273ecd8b8
Nysera <73109965+Nysera@users.noreply.github.com>
2020-12-10 16:10:26 +1000

    Minor markup changes

--- assets/js/templates/add.book.template.js
+++ assets/js/templates/add.book.template.js
@@ -3,33 +3,33 @@ const addBookTemplate = function(){
         <div class="form-modal_header">
             <h2>Add a new book</h2>
             <button id="close-modal" class="btn">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 16 16"><path d="M13.957 3.457l-1.414-1.414-4.543 4.543-4.543-4.543-1.414 1.414 4.543 4.543-4.543 4.543 1.414 1.414 4.543-4.543 4.543 4.543 1.414-1.414-4.543-4.543z"></path></svg>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path d="M13.957 3.457l-1.414-1.414-4.543 4.543-4.543-4.543-1.414 1.414 4.543 4.543-4.543 4.543 1.414 1.414 4.543-4.543 4.543 4.543 1.414-1.414-4.543-4.543z"></path></svg>
             </button>
         </div>
         <form autocomplete="off">
             <div class="input-wrapper">
                 <label for="title">Title:</label>
-                <input type="text" id="title" name="title">
+                <input type="text" id="title" name="title" placeholder="The Sentinel">
             </div>
             <div class="input-wrapper">
                 <label for="author">Author:</label>
-                <input type="text" id="author" name="author">
+                <input type="text" id="author" name="author" placeholder="Lee Child, Andrew Child">
             </div>
             <div class="input-wrapper">
                 <label for="pages">Number Of Pages:</label>
-                <input type="number" id="pages" name="pages">
+                <input type="number" id="pages" name="pages" placeholder="400">
             </div>
             <div class="input-wrapper">
                 <label for="image-url">Cover Image URL:</label>
-                <input type="text" id="image-url" name="image-url">
+                <input type="text" id="image-url" name="image-url" placeholder="Cover image from booktopia.com.au">
             </div>
             <div class="input-wrapper">
                 <label for="read-status">Read Status:</label>
                 <select id="read-status" name="read-status">
                     <option value="">-- Please select an option --</option>
-                    <option value="read">Read</option>
-                    <option value="in progress">In Progress</option>
-                    <option value="not read">Not Read</option>
+                    <option value="Read">Read</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Not Read">Not Read</option>
                 </select>
             </div>
             <div class="input-wrapper submit">
